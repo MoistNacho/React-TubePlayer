c0b1dc33b774e05795c721585bb63f28337801be
Moist_Nacho <hajo1563@gmail.com>
2020-12-20 16:36:55 +0900

    styles update

--- src/components/videoList/VideoList.jsx
+++ src/components/videoList/VideoList.jsx
@@ -6,15 +6,17 @@ class videoList extends Component {
     render() {
         let {videos, isLoading} = this.props;
         return (
-            <ul className={styles.videoList}>
-                {
-                    isLoading ?
-                    videos.map((video)=>{
-                        return <Video key={video.etag} video={video} VideoInfo={this.props.VideoInfo} />
-                    })
-                    :
-                    `Loading...`
-                } 
+            <ul className={styles.list}>
+                <div className={styles.videos}>
+                    {
+                        isLoading ?
+                        videos.map((video)=>{
+                            return <Video key={video.etag} video={video} VideoInfo={this.props.VideoInfo} />
+                        })
+                        :
+                        `Loading...`
+                    } 
+                </div> 
             </ul>
         );
     }
